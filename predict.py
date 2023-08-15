0eeafe8299eb94b240ec8c4fdf22c704f57877b4
Callan <callan@goldeneye.app>
2023-08-15 08:08:14 -0700

    hotfix

--- predict.py
+++ predict.py
@@ -10,9 +10,9 @@ class Predictor(BasePredictor):
         self.detector = dlib.get_frontal_face_detector()
 
     def predict(self, image: CogPath = Input(description="Path to the image")) -> CogPath:
-        img = dlib.load_rgb_image(str(image)) # Convert the CogPath to a string
+        img = dlib.load_rgb_image(str(image))
         dets = self.detector(img)
-        faces = [{"x": (d.left() + d.right()) // 2, "y": (d.top() + d.bottom()) // 2, "size": d.width()} for d in dets]
+        faces = [{"x": d.left(), "y": d.top(), "size": d.width()} for d in dets]
 
         out_path = CogPath(tempfile.mkdtemp()) / 'output.json'
         with open(out_path, 'w') as f:
